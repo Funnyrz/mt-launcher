fb7b3b79a127f36691e8c1fb90f51dd08b27bc38
funnyrz <746108479@qq.com>
2022-07-15 15:32:03 +0800

    first commit

---
+++ src/renderer/components/Index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="parent">
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+    <div class="child">
+      <img src="static/app.png" alt="icon" width="150">
+      <div class="desc">
+        啊啊啊啊
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index-page',
+  components: {},
+  methods: {}
+}
+</script>
+
+<style>
+.parent {
+  display: flex;
+  flex-flow: row wrap;
+  align-content: center;
+}
+
+.child {
+  margin: 10px 0;
+  box-sizing: border-box;
+  flex: 0 0 25%;
+  text-align: center;
+}
+
+.desc {
+  color: aliceblue
+}
+</style>
