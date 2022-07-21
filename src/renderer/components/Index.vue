58485028b57b8e0a2883052f401b0c8197fb560f
funnyrz <746108479@qq.com>
2022-07-21 16:53:50 +0800

    添加右键菜单功能 1.支持打开软件 2.支持打开软件所在目录 3.移除对软件的管理 4.卸载软件(未实现)

--- src/renderer/components/Index.vue
+++ src/renderer/components/Index.vue
@@ -4,7 +4,8 @@
       <el-input v-model="keywords" placeholder="搜索" @input="search"></el-input>
     </div>
     <div class="parent" v-if="!isSearch">
-      <div class="child" v-for="app in apps" v-on:dblclick="openApp(app)">
+      <div class="child" v-for="(app,index) in apps" v-on:dblclick="openApp(app)"
+           @contextmenu.prevent="$refs.ctxMenu.open(this,{'index':index,'data':app})">
         <div class="app">
           <img :src="app.icon" alt="icon">
           <div class="desc">
@@ -23,6 +24,12 @@
         </div>
       </div>
     </div>
+    <context-menu id="context-menu" ref="ctxMenu">
+      <li @click="openApp($refs.ctxMenu.locals.data)">打开</li>
+      <li @click="openDir()">打开文件夹</li>
+      <li @click="removeApp($refs.ctxMenu.locals.index)">删除</li>
+      <li @click="uninstallApp($refs.ctxMenu.locals.index)">卸载</li>
+    </context-menu>
   </div>
 </template>
 
@@ -32,10 +39,11 @@ const iconPromise = require('icon-promise');
 const {lnk} = require('./util/dbUtil')
 const Fuse = require('fuse.js')
 import {pinyin} from 'pinyin-pro';
+import contextMenu from 'vue-context-menu'
 
 export default {
   name: 'index-page',
-  components: {},
+  components: {contextMenu},
   data() {
     return {
       apps: [],
@@ -45,10 +53,31 @@ export default {
   },
   created() {
     this.apps = this.getLnkData()
-    console.log(this.apps)
     this.dropFile()
   },
   methods: {
+    uninstallApp() {
+      this.$message.error({
+        showClose: true,
+        message: '这是一条消息提示',
+        duration: '2000'
+      });
+    },
+    removeApp(index) {
+      this.$confirm('此操作只会移除当前应用对其他软件的启动管理,不会卸载或删除.确定要继续吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.apps.splice(index, 1)
+        lnk.set('apps', this.apps)
+      }).catch(() => {
+
+      });
+    },
+    openDir() {
+      shell.showItemInFolder(this.$refs.ctxMenu.locals.data.exePath)
+    },
     search() {
       if (this.keywords === '') {
         this.isSearch = false
@@ -56,11 +85,10 @@ export default {
       } else {
         const options = {
           includeScore: true,
-          keys: ['appName','word']
+          keys: ['appName', 'word']
         }
         const fuse = new Fuse(this.getLnkData(), options)
         this.apps = fuse.search(this.keywords)
-        console.log(this.apps)
         this.isSearch = true
       }
     },
@@ -84,7 +112,7 @@ export default {
           this.getIconInfo(exePath).then((data) => {
             let appName = name.replace(".lnk", '')
             const word = pinyin(appName, {pattern: 'first', toneType: 'none'})
-            let app = {"appName": appName, "lnk": filePath, "icon": data, "word": word}
+            let app = {"appName": appName, "lnk": filePath, "icon": data, "word": word, "exePath": exePath}
             this.apps.splice(this.apps.length, 1, app)
             lnk.set('apps', this.apps)
           })
@@ -145,4 +173,33 @@ export default {
   margin: auto;
 }
 
+.ctx-menu {
+  position: absolute;
+  top: 100%;
+  z-index: 1000;
+  display: none;
+  /*min-width: 70px;*/
+  font-size: 1rem;
+  color: #373a3c;
+  text-align: left;
+  list-style: none;
+  -webkit-background-clip: padding-box;
+  background-clip: padding-box;
+  border: 1px solid rgba(0, 0, 0, .15);
+  border-radius: .3rem;
+  -moz-box-shadow: 0 0 5px #ccc;
+  -webkit-box-shadow: 0 0 5px #ccc;
+  box-shadow: 0 0 5px #ccc;
+  background-color: #ecf0f1;
+  font-weight: bold;
+}
+
+.ctx-menu > li {
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.ctx-menu > li:hover {
+  background: #9fb8ad;
+}
 </style>
