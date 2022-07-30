8e175e21151b84eb368960fc56411f4862faa5a5
funnyrz <746108479@qq.com>
2022-07-30 21:51:30 +0800

    升级脚手架,原来代码保存为分支

--- src/renderer/components/util/dbUtil.js
+++
@@ -1,5 +0,0 @@
-const LNDB = require('lndb')
-const {app} = require('electron').remote
-const db = new LNDB(app.getPath('userData'))
-const appInfoDb = db.init('appInfo')
-module.exports = {appInfoDb}
